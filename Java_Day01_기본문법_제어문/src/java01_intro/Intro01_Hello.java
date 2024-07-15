e4d8489504a57e033aee8dd4e737b4c9cbd2fc8a
jwkim0405 <jeongwoocode@gmail.com>
2024-07-15 12:06:33 +0900

    first java day

---
+++ Java_Day01_기본문법_제어문/src/java01_intro/Intro01_Hello.java
@@ -0,0 +1,24 @@
+package java01_intro;
+import java.util.Scanner;
+
+public class Intro01_Hello {
+	public class right() {
+		
+	}
+	// 자동완성을 적극적으로 활용!!
+	// main + ctrl + spacebar
+	public static void main(String[] args) {
+		// sysout + ctrl + spacebar
+//		System.out.println("Hello, World!");	
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		// 오른쪽으로 쭉 갔다가
+		// 아래로 쭉 갔다가
+		// 왼쪽으로 쭉 갔다가
+		// 위로 쭉
+		// ... 반복
+		
+		
+	}
+}
